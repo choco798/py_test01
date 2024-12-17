580b9d6727f02c2f8a383ca829018ea7f286c27d
choco798 <74724253+choco798@users.noreply.github.com>
2024-12-17 17:14:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pyxel
 
 class App:
     def __init__(self):
-    pyxel.init(160, 120)
-    self.x = 0
-    pyxel.run(self.update, self.draw)
+        pyxel.init(160, 120)
+        self.x = 0
+        pyxel.run(self.update, self.draw)
 
 def update(self):
     self.x = (self.x + 1) % pyxel.width
